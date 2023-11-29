5d49d6a8a74a8ddb325ab01fd7d109b11026d6c4
BrianPizz <bpizzimentijr@outlook.com>
2023-11-29 15:26:18 -0800

    user, product, and order models

---
+++ server/models/Order.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require('../utils/dateformat');
+
+const orderSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  products: [
+    {
+      product: { type: Schema.Types.ObjectId, ref: "User", required: true },
+      quantity: { type: Number, required: true }
+    },
+  ],
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now(),
+    get: (date) => dateFormat(date)
+  },
+});
+
+const Order = model('Order', orderSchema);
+
+module.exports = Order;
---
+++ server/models/Product.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    default: 0,
+  },
+  image: {
+    type: String,
+    trim: true,
+  },
+});
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
