482888fb27b7e6a167ade2ddeafe406cc966b3b8
BrianPizz <bpizzimentijr@outlook.com>
2024-01-01 11:34:54 -0800

    client authorization setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,38 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up client to execute the `authLink` middleware prior to making the request to GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <ApolloProvider client={client}>
+      <div className="text-3xl">Hello World!</div>
+    </ApolloProvider>
+  );
 }
