2e0140222ff0f42d9e287f3d57c723468f516e3c
BrianPizz <bpizzimentijr@outlook.com>
2024-01-04 10:18:41 -0800

    updated header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,13 @@
 const Header = () => {
-    return (
-        <div className="flex bg-slate-200 p-3 justify-between">
-            <p className="font-bold text-2xl">Sound Harbor</p>
-            <button className="text-2xl">My Account</button>
-        </div>
-    )
-}
+  return (
+    <div className="flex bg-slate-200 p-3 justify-between">
+      <p className="font-bold text-2xl">Sound Harbor</p>
+      <div>
+        <button className="text-2xl mx-2">My Account</button>
+        <button className="text-2xl mx-2">Cart</button>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
