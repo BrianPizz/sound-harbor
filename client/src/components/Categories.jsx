843492bbfa83a1e9dc3d4600c024af206010e724
BrianPizz <bpizzimentijr@outlook.com>
2024-01-05 10:25:33 -0800

    category styling

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -4,9 +4,9 @@ const Categories = ({ categories, title }) => {
     }
 
   return (
-    <div className="w-3/4 bg-slate-300">
+    <div className="w-3/4 mt-6 py-2">
       <p className="text-2xl font-semibold text-center">{title}</p>
-      <div className="flex justify-between px-2">
+      <div className="flex justify-between px-2 mt-4">
         {categories.map(category => (
           <button key={category._id}>{category.name}</button>
         ))}
