aa6e5004234372408761b0c905290c622bff5c0d
BrianPizz <bpizzimentijr@outlook.com>
2024-01-06 17:44:29 -0800

    category styling

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,18 +1,18 @@
 const Categories = ({ categories, title }) => {
-    if (!categories.length){
-        return <p>No Categories</p>
-    }
+  if (!categories.length) {
+    return <p>No Categories</p>;
+  }
 
   return (
     <div className="w-3/4 mt-6 py-2">
       <p className="text-2xl font-semibold text-center">{title}</p>
       <div className="flex justify-between px-2 mt-4">
-        {categories.map(category => (
+        {categories.map((category) => (
           <button key={category._id}>{category.name}</button>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Categories
+export default Categories;
