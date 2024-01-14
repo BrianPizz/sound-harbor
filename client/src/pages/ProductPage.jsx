f7474b47575cf1ab4e108ca5e29488b396d0ac60
BrianPizz <bpizzimentijr@outlook.com>
2024-01-14 13:27:46 -0800

    product page product query

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_PRODUCT } from "../utils/queries";
 
 const ProductPage = () => {
+const { productId } = useParams();
+
+const {loading, data } = useQuery(QUERY_PRODUCT, {
+    variables: {productId},
+});
+
+if (loading) return <div>Loading...</div>;
+
+const product = data.product;
+
+console.log(product)
+
     return <div>Product Page</div>;
   };
   
