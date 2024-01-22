c02f5770ff2ba5e3c4ffc86e0d76beab213f7b2c
BrianPizz <bpizzimentijr@outlook.com>
2024-01-22 10:16:10 -0800

    category styling

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -10,7 +10,7 @@ const Categories = ({ categories, title }) => {
       <p className="text-2xl font-semibold text-center">{title}</p>
       <div className="flex justify-between px-2 mt-4">
         {categories.map((category) => (
-          <button key={category._id}>
+          <button key={category._id} className="p-2 transition-transform transform hover:scale-105 hover:bg-blue-600 hover:text-white rounded-md">
             <Link to={`/category/${category._id}`}>{category.name}</Link>
           </button>
         ))}
