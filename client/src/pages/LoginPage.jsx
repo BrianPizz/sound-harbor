09f2e9d7035d2c50da583a2cc2fb2377f7d6e4ef
BrianPizz <bpizzimentijr@outlook.com>
2024-01-23 10:06:38 -0800

    set up log in page component

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
+
+const Login = () => {
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { loading, error, data }] = useMutation(LOGIN_USER);
+
+  // handle form input chhange
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  // call log in user mutation on sumbit
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await login({
+        variables: { ...formState },
+      });
+      Auth.login(data.login.token);
+    } catch (e) {
+      console.error(e);
+    }
+
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <div>
+        login
+    </div>
+  )
+};
+
+export default Login;
