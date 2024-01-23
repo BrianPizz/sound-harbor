1b8f801190db9cc5ce03974e72a4fe11fd4215f7
BrianPizz <bpizzimentijr@outlook.com>
2024-01-23 10:14:03 -0800

    updated home page to direct login

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,27 @@
+import React from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_CATEGORIES } from "../utils/queries";
 import Categories from "../components/Categories";
+import Auth from "../utils/auth";
+import Login from "./LoginPage";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_CATEGORIES);
   const categories = data?.categories || [];
+  const isLoggedIn = Auth.loggedIn();
 
   return (
-    <div className="flex justify-center">
-      {loading ? (
-        <div>Loding...</div>
+    <div>
+      {isLoggedIn ? (
+        <div className="flex justify-center">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <Categories categories={categories} title="Categories" />
+          )}
+        </div>
       ) : (
-        <Categories categories={categories} title="Categories" />
+        <Login />
       )}
     </div>
   );
