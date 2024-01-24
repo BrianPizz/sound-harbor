6241494f6a93ccb6c1b6c9373828fc8e82b32a8e
BrianPizz <bpizzimentijr@outlook.com>
2024-01-24 10:56:37 -0800

    logout added to nav

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,26 @@
+import Auth from "../utils/auth";
+
 const Header = () => {
+  const isLoggedIn = Auth.loggedIn();
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <div className="flex bg-slate-200 p-3 justify-between">
       <p className="font-bold text-2xl">Sound Harbor</p>
+        {isLoggedIn ? (
       <div>
-        <button className="text-2xl mx-2">My Account</button>
-        <button className="text-2xl mx-2">Cart</button>
+            <button className="text-2xl mx-2">My Account</button>
+            <button className="text-2xl mx-2">Cart</button>
+            <button className="text-2xl mx-2" onClick={logout}>Logout</button>
       </div>
+        ) : (
+        <div>
+            <p className="text-2xl mx-2">Log in</p>
+        </div>
+        )}
     </div>
   );
 };
