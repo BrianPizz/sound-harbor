da480f889d5d980615ca2785a570ac6b615cca42
BrianPizz <bpizzimentijr@outlook.com>
2024-01-25 17:33:52 -0800

    login page styling

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
 
 const Login = () => {
+  const navigate = useNavigate();
   const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { loading, error, data }] = useMutation(LOGIN_USER);
 
-  // handle form input chhange
+  // handle form input change
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormState({
@@ -17,7 +18,7 @@ const Login = () => {
     });
   };
 
-  // call log in user mutation on sumbit
+  // call login user mutation on submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -26,7 +27,7 @@ const Login = () => {
       });
       Auth.login(data.login.token);
       // Redirect to the home page after successful login
-      history.push("/");
+      navigate.push("/");
     } catch (e) {
       console.error(e);
     }
@@ -37,40 +38,51 @@ const Login = () => {
     });
   };
 
-  return <div>
-        <div>
-      <h2>Login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={formState.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            name="password"
-            value={formState.password}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit" disabled={loading}>
-          {loading ? 'Logging in...' : 'Login'}
-        </button>
-      </form>
-      {error && <p>Error: {error.message}</p>}
-      <p>
-        Don't have an account? <Link to="/">Sign up here</Link>.
-      </p>
+  return (
+    <div className="flex justify-center h-screen bg-sky-600">
+      <div className="text-center rounded-md bg-white m-auto w-1/2 p-6">
+        <p className="text-4xl my-6 font-bold">Sound Harbor</p>
+        <h2 className="text-2xl mb-3 font-light text-slate-600">Sign in</h2>
+        <form onSubmit={handleFormSubmit} className="text-left">
+          <div className="mb-3 transition-all duration-300">
+            <label htmlFor="email" className="text-slate-500">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formState.email}
+              onChange={handleChange}
+              required
+              className="w-full p-2 border border-slate-300 rounded-md focus:outline-none focus:border-blue-500 transition-all duration-300"
+            />
+          </div>
+          <div className="mb-3 transition-all duration-300">
+            <label htmlFor="password" className="text-slate-500">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formState.password}
+              onChange={handleChange}
+              required
+              className="w-full p-2 border border-slate-300 rounded-md focus:outline-none focus:border-blue-500 transition-all duration-300"
+            />
+          </div>
+          <button
+            type="submit"
+            disabled={loading}
+            className="w-full bg-sky-700 text-white mt-2 p-2 rounded-sm hover:bg-sky-800 transition-all duration-300"
+          >
+            {loading ? "Logging in..." : "Login"}
+          </button>
+        </form>
+        {error && <p className="text-red-500">Error: {error.message}</p>}
+        <p className="mt-3 text-slate-600">
+          Don't have an account? <Link to="/" className="underline text-sky-500">Sign up here</Link>.
+        </p>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Login;
